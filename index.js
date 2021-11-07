9a86522b92b6a8e8558bcd7152d1fd143998fd49
Dustin Crabtree <dustin.crabtree@tridius.co>
2021-11-07 01:34:09 -0500

    Add dynamic querystring builder + config vars

--- index.js
+++ index.js
@@ -1,100 +1,107 @@
-var request = require('request');
+const request = require('request');
 const notifier = require('node-notifier');
 
-var options = {
-  'method': 'GET',
-  'url': 'https://www.apple.com/shop/fulfillment-messages?parts.0=MKGR3LL%2FA&parts.1=MKGP3LL%2FA&parts.2=MKGT3LL%2FA&parts.3=MKGQ3LL%2FA&parts.4=MMQX3LL%2FA&parts.5=MKH53LL%2FA&parts.6=MK1E3LL%2FA&parts.7=MK183LL%2FA&parts.8=MK1F3LL%2FA&parts.9=MK193LL%2FA&parts.10=MK1H3LL%2FA&parts.11=MK1A3LL%2FA&parts.12=MYD92LL%2FA&searchNearby=true&store=R172'
-};
-
-const favoriteSilver = 'MMQX3LL/A'
-const favorateSpaceGray = 'MKH53LL/A'
-const control = 'MYD92LL/A'
+const favorites = ['MMQX3LL/A', 'MKH53LL/A', 'MK1A3LL/A', 'MK1H3LL/A'];
+const control = 'MYD92LL/A';
+const storeNumber = 'R172';
+const timeZone = 'America/Denver';
+const state = 'CO';
 
 const skus = {
-  'MKGR3LL/A': '14\" Si, Base',
-  'MKGP3LL/A': '14\" SG, Base',
-  'MKGT3LL/A': '14\" Si, Better',
-  'MKGQ3LL/A': '14\" SG, Better',
-  'MMQX3LL/A': '14\" Si, Ultimate',
-  'MKH53LL/A': '14\" SG, Ultimate',
-  'MK1E3LL/A': '16\" Si, Base',
-  'MK183LL/A': '16\" SG, Base',
-  'MK1F3LL/A': '16\" Si, Better',
-  'MK193LL/A': '16\" SG, Better',
-  'MK1H3LL/A': '16\" Si, Best',
-  'MK1A3LL/A': '16\" SG, Best',
-  'MYD92LL/A': '13\" Control'
-}
+	'MKGR3LL/A': '14" Si, Base',
+	'MKGP3LL/A': '14" SG, Base',
+	'MKGT3LL/A': '14" Si, Better',
+	'MKGQ3LL/A': '14" SG, Better',
+	'MMQX3LL/A': '14" Si, Ultimate',
+	'MKH53LL/A': '14" SG, Ultimate',
+	'MK1E3LL/A': '16" Si, Base',
+	'MK183LL/A': '16" SG, Base',
+	'MK1F3LL/A': '16" Si, Better',
+	'MK193LL/A': '16" SG, Better',
+	'MK1H3LL/A': '16" Si, Best',
+	'MK1A3LL/A': '16" SG, Best',
+	'MK1A3LL/A': '16" SG, Ultimate',
+	'MK1H3LL/A': '16" Si, Ultimate',
+	'MYD92LL/A': '13" Control',
+};
+
+const query =
+	Object.keys(skus)
+		.map((k, i) => `parts.${i}=${encodeURIComponent(k)}`)
+		.join('&') + `&searchNearby=true&store=${storeNumber}`;
+
+var options = {
+	method: 'GET',
+	url: 'https://www.apple.com/shop/fulfillment-messages?' + query,
+};
 
 request(options, function (error, response) {
-  if (error) throw new Error(error);
-  const body = JSON.parse(response.body)
-  const storesArray = body.body.content.pickupMessage.stores;
+	if (error) throw new Error(error);
 
-  let skuCounter = {}
+	const body = JSON.parse(response.body);
+	const storesArray = body.body.content.pickupMessage.stores;
+	let skuCounter = {};
+	let hasStoreSearchError = false;
 
-  let hasStoreSearchError = false
-  const statusArray = storesArray
-    .flatMap((store) => {
-      const name = store.storeName;
-      const state = store.state;
+	const statusArray = storesArray
+		.flatMap((store) => {
+			if (state && state !== store.state) return null;
 
-      // only check Colorado stores
-      if (state !== 'CO') return null;
+			const name = store.storeName;
+			let productStatus = [];
 
-      let productStatus = []
+			for (const [key, value] of Object.entries(skus)) {
+				const product = store.partsAvailability[key];
 
-      for (const [key, value] of Object.entries(skus)) {
-        const product = store.partsAvailability[key]
-        hasStoreSearchError = product.storeSearchEnabled !== true;
+				hasStoreSearchError = product.storeSearchEnabled !== true;
 
-        if (key === control && hasStoreSearchError !== true) {
-          hasStoreSearchError = product.pickupDisplay !== 'available'
-        } else {
-          productStatus.push(`${value}: ${product.pickupDisplay}`)
-          if (product.pickupDisplay !== 'unavailable') {
-            console.log(`${value} in stock at ${store.storeName}`)
-            let count = !!skuCounter[key] ? skuCounter[key] : 0
-            count += 1
-            skuCounter[key] = count
-          }
-        }
-      }
+				if (key === control && hasStoreSearchError !== true) {
+					hasStoreSearchError = product.pickupDisplay !== 'available';
+				} else {
+					productStatus.push(`${value}: ${product.pickupDisplay}`);
 
-      return {
-        name: name,
-        products: productStatus
-      }
-    })
-    .filter((n) => n)
+					if (product.pickupDisplay !== 'unavailable') {
+						console.log(`${value} in stock at ${store.storeName}`);
+						let count = skuCounter[key] ?? 0;
+						count += 1;
+						skuCounter[key] = count;
+					}
+				}
+			}
 
-  let hasError = hasStoreSearchError;
+			return {
+				name: name,
+				products: productStatus,
+			};
+		})
+		.filter((n) => n);
 
-  let inventory = []
-  for (const [key, value] of Object.entries(skuCounter)) {
-    inventory.push(`${skus[key]}: ${value}`)
-  }
+	let hasError = hasStoreSearchError;
 
-  let inventoryString = inventory.join()
-  console.log(inventoryString);
+	const inventory = Object.entries(skuCounter)
+		.map(([key, value]) => `${skus[key]}: ${value}`)
+		.join(' | ');
 
-  let hasUltimate = !!skuCounter[favoriteSilver] || !!skuCounter[favorateSpaceGray]
-  let notificationMessage
-  if (inventory.length > 0) {
-    notificationMessage = `${hasUltimate ? 'FOUND ULTIMATE! ' : ''}Some models found: ${inventoryString}`
-  } else {
-    console.log(statusArray);
-    notificationMessage = 'No models found.'
-  }
+	console.log(inventory);
 
-  const message = hasError ? 'Possible error?' : notificationMessage;
-  notifier.notify({
-    title: 'MacBook Pro Availability',
-    message: message,
-    sound: hasError || inventory.length > 0,
-    timeout: false
-  });
+	let hasUltimate = Object.keys(skuCounter).some((r) => favorites.indexOf(r) >= 0);
+	let notificationMessage;
 
-  // Log time at end
-  console.log(new Date().toLocaleString('en-US', { timeZone: 'America/Denver' }));
+	if (inventory) {
+		notificationMessage = `${hasUltimate ? 'FOUND ULTIMATE! ' : ''}Some models found: ${inventory}`;
+	} else {
+		console.log(statusArray);
+		notificationMessage = 'No models found.';
+	}
+
+	const message = hasError ? 'Possible error?' : notificationMessage;
+	notifier.notify({
+		title: 'MacBook Pro Availability',
+		message: message,
+		sound: hasError || inventory,
+		timeout: false,
+	});
+
+	// Log time at end
+	console.log(new Date().toLocaleString('en-US', { timeZone }));
 });
