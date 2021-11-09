c48c0456321e5b696fe2bedb975f41afb20fc952
Björn Marschollek <git@muffix.de>
2021-11-09 22:39:18 +0100

    Parametrise the SKU list and favourites
    We can dynamically create SKU lists and favourites. The SKUs all have
    the same format of 5 letters, a country code, and end in `/A`. We don't
    need to spell them out for each country. It's enough to configure the
    code once.

--- constants.js
+++ constants.js
@@ -1,46 +1,22 @@
-const SKUS = {
-  'MKGR3LL/A': '14" Si, Base',
-  'MKGP3LL/A': '14" SG, Base',
-  'MKGT3LL/A': '14" Si, Better',
-  'MKGQ3LL/A': '14" SG, Better',
-  'MMQX3LL/A': '14" Si, Ultimate',
-  'MKH53LL/A': '14" SG, Ultimate',
-  'MK1E3LL/A': '16" Si, Base',
-  'MK183LL/A': '16" SG, Base',
-  'MK1F3LL/A': '16" Si, Better',
-  'MK193LL/A': '16" SG, Better',
-  'MK1H3LL/A': '16" Si, Best',
-  'MK1A3LL/A': '16" SG, Best',
-  'MK233LL/A': '16" SG, Ultimate',
-  'MMQW3LL/A': '16" Si, Ultimate',
-  'MYD92LL/A': '13" Control',
-};
-
-const SKUS_AUSTRALIA = {
-  'MKGR3X/A': '14" M1 Pro 8 Core CPU 14 Core GPU 512GB Silver',
-  'MKGP3X/A': '14" M1 Pro 8 Core CPU 14 Core GPU 512GB Space Grey',
-  'MKGT3X/A': '14" M1 Pro 10 Core CPU 16 Core GPU 1TB Silver',
-  'MKGQ3X/A': '14" M1 Pro 10 Core CPU 16 Core GPU 1TB Space Grey',
-  'MMQX3X/A': '14" M1 Max 10 Core CPU 32 Core GPU 2TB Silver, Ultimate',
-  'MKH53X/A': '14" M1 Max 10 Core CPU 32 Core GPU 2TB Space Grey, Ultimate',
-  'MK1H3X/A': '16" M1 Max 10 Core CPU 32 Core GPU 1TB Silver',
-  'MK1A3X/A': '16" M1 Max 10 Core CPU 32 Core GPU 1TB Space Grey',
-  'MMQW3X/A': '16" M1 Max 10 Core CPU 32 Core GPU 4TB Silver, Ultimate',
-  'MK233X/A': '16" M1 Max 10 Core CPU 32 Core GPU 4TB Space Grey, Ultimate',
-  'MK1F3X/A': '16" M1 Pro 10 Core CPU 16 Core GPU 1TB Silver',
-  'MK193X/A': '16" M1 Pro 10 Core CPU 16 Core GPU 1TB Space Grey',
-  'MK1E3X/A': '16" M1 Pro 10 Core CPU 16 Core GPU 512GB Silver',
-  'MK183X/A': '16" M1 Pro 10 Core CPU 16 Core GPU 512GB Space Grey',
-}
-
 const COUNTRIES = {
-  US: "",
-  CA: "/ca",
-  AU: "/au",
+  US: {
+    "storePath": "",
+    "skuCode": "LL",
+  },
+  CA: {
+    "storePath": "/ca",
+    "skuCode": "LL",
+  },
+  AU: {
+    "storePath": "/au",
+    "skuCode": "X",
+  },
+  DE: {
+    "storePath": "/de",
+    "skuCode": "D",
+  },
 };
 
 module.exports = {
-  SKUS: SKUS,
-  SKUS_AUSTRALIA: SKUS_AUSTRALIA,
   COUNTRIES: COUNTRIES,
 };
